ef980c369782d1752cd88eee70bd3f4d6da60658
Diego-PG <diegoprieto5@gmail.com>
2019-02-11 18:53:00 +0100

    dpi scripts

---
+++ monitor_fhd.sh
@@ -0,0 +1,3 @@
+xfconf-query -c xsettings -p /Xft/DPI -s 100
+feh --bg-fill ~/.config/i3/wallpaper.jpg
+i3-msg restart 
---
+++ monitor_uhd.sh
@@ -0,0 +1,3 @@
+xfconf-query -c xsettings -p /Xft/DPI -s 160
+feh --bg-fill ~/.config/i3/wallpaper-4k.jpg
+i3-msg restart
